d58b60960f42c61a7f969638089944c684c55b75
hexeva <patrick79.g@gmail.com>
2022-01-25 17:31:02 +0100

    building data structure

--- index.php
+++ index.php
@@ -4,12 +4,90 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="data.php">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style.css">
     <title>GOOGLE FAQ</title>
 </head>
 <body>
+
+<!-- DATAS -->
+<?php
+    $google_faqs =[
+
+        'faq_1' => [
+            'question' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+
+            'answers' => [
+                "La recente <a href=\"#\"> decisione della Corte di giustizia dell'Unione europea </a>  ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
+
+
+                "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.",
+
+                "Per presentare una richiesta di rimozione, compila questo <a href=\"#\">modulo web.</a> Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.",
+
+                "Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.",
+
+                "Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.", 
+
+            ],
+        ],
+
+        'faq_2' => [
+            'question' => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+
+            'answers' => [
+                "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.",
+                "Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.",
+
+                "Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel <a href=\"#\">Centro Google per la sicurezza online.</a>",
+
+                "<a href=\"#\">Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.",
+            ],
+        ],
+
+        'faq_3' => [
+            'question' => "Perché il mio account è associato a un paese?",
+
+            'answers' => [
+                "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
+
+                "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:",
+
+                "Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.",
+
+                "Google LLC, con sede negli Stati Uniti, per il resto del mondo",
+
+                "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali",
+
+                "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.",
+            ],
+        ],
+
+        'faq_4' => [
+            'question' => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+
+            'answers' => [
+                'risposta uno',
+                'risposta due',
+                'risposta tre'
+            ],
+        ],
+
+        'faq_4' => [
+            'question' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+
+            'answers' => [
+                'risposta uno',
+                'risposta due',
+                'risposta tre'
+            ],
+        ],
+    ];
+
+    // var_dump($google_faqs['faq_1']);
+
+?>
+<!-- END DATAS -->
 
 
     <!-- HEADER -->
@@ -56,11 +134,15 @@
     <main>
         <div class="container">
 
-            <?php foreach($google_faqs as $faq => $topics){ ?>
+            <?php foreach ($google_faqs as $faqs) { ?>
 
                 <div class="faq">
-                    <h2>[DOMANDE]</h2>
-                    <p>[RISPOSTE]</p>
+                    <h2> <?php echo $faqs['question']; ?></h2>
+
+                    <?php foreach($faqs['answers'] as $one_answer){ ?>
+                        <p> <?php echo $one_answer; ?></p>
+
+                    <?php }?>
                 </div>
 
             <?php } ?>
