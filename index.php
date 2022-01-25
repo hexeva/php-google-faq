0c3d5a92c160dea4bc5381250f570b5b94f16310
hexeva <patrick79.g@gmail.com>
2022-01-25 18:16:13 +0100

    adding footer's elements

--- index.php
+++ index.php
@@ -51,13 +51,13 @@
             'answers' => [
                 "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
 
-                "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:",
+                "1  La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:",
 
-                "Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.",
+                "   a.      Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.",
 
-                "Google LLC, con sede negli Stati Uniti, per il resto del mondo",
+                "   b.      Google LLC, con sede negli Stati Uniti, per il resto del mondo",
 
-                "La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali",
+                "2  La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali",
 
                 "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.",
             ],
@@ -67,21 +67,20 @@
             'question' => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
 
             'answers' => [
-                'risposta uno',
-                'risposta due',
-                'risposta tre'
+                "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi <a href=\"#\">contattare il webmaster</a> del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google,<a href=\"#\">fai clic qui.</a>  Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile <a href=\"#\">visitare la nostra pagina di assistenza per avere ulteriori informazioni.</a> ",
             ],
         ],
 
-        'faq_4' => [
+        'faq_5' => [
             'question' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
 
             'answers' => [
-                'risposta uno',
-                'risposta due',
-                'risposta tre'
+                "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di <a href=\"#\">URL referrer<a/>. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <a href=\"#\">qui<a/>. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.",
+                
             ],
         ],
+
+    
     ];
 
     // var_dump($google_faqs['faq_1']);
@@ -153,8 +152,37 @@
 
     <!-- FOOTER -->
     <footer>
-        IO SONO IL FOOTER
-        <a href=""></a>
+        <div class="container">
+            <div class="footer-bar">
+                <div class="footer-credits">
+                    <ul class="credits">
+                        <li>
+                            <a href="#">Google</a>
+                        </li>
+                        <li>
+                            <a href="#">Tutto su Google</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy</a>
+                        </li>
+                        <li>
+                            <a href="#">Termini</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+                <div class="footer-search">
+                    <form action="">
+                        <select name="lingue" id="lingue">
+                            <option value="italiano">Italiano</option>
+                            <option value="english">English</option>
+                            <option value="espaniol">Espaniol</option>
+                            <option value="suomi">Suomi</option>
+                        </select>
+                    </form>
+                </div>
+            </div>
+        </div>
     </footer>
     <!-- END FOOTER -->
 </body>
