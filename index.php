ee3df1f391b93f02a6397afea122dbf7bcaabf89
hexeva <patrick79.g@gmail.com>
2022-01-25 15:14:24 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+    <title>GOOGLE FAQ</title>
+</head>
+<body>
+
+    <!-- HEADER -->
+    <header>
+        
+        <section class="header-top">
+            <div class="logo-title">
+                <div class="logo">
+                    <img src="img/google.jpeg" alt="google logo">
+                </div>
+                <span class="title">
+                    <h2>Privacy e termini</h2>
+                </span>
+            </div>
+            <div class="top-menu">
+                <a href="#">
+                    <i class="fas fa-bars"></i>
+                </a>
+            </div>
+        </section>
+        <section class="header-bottom">
+            <ul class="header-menu">
+                <li>
+                    <a href="#">Introduzione</a>
+                </li>
+                <li>
+                    <a href="#">Norme sulla Privacy</a>
+                </li>
+                <li>
+                    <a href="#">Termini di servizio</a>
+                </li>
+                <li>
+                    <a href="#">Tecnologie</a>
+                </li>
+                <li>
+                    <a href="#">Domande frequenti</a>
+                </li>
+            </ul>
+        </section>
+    </header>
+    <!-- END HEADER -->
+
+    <!-- MAIN -->
+    <main>
+        <div class="container"></div>
+    </main>
+    <!-- END MAIN -->
+
+    <!-- FOOTER -->
+    <!-- END FOOTER -->
+</body>
+</html>
